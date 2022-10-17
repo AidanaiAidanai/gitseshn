4198d1642a59100f079147563984e94288adb73f
Admin <aidanamamatberdieva2001@gmail.com>
2022-10-17 07:49:16 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 6;
+        int v = 7;
+        System.out.println(a+v);
+    }
+}
